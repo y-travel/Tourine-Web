79e7a06e3b8d275166ae5ca585c8bea70aa68076
mohammad <m.roosta@outlook.com>
2017-10-27 18:54:26 +0330

    #110 Add translation into trh-input placeholder

--- src/app/@theme/components/trh-input.component.ts
+++ src/app/@theme/components/trh-input.component.ts
@@ -1,11 +1,22 @@
-import { Directive } from "@angular/core";
+import { Directive, AfterViewInit, Input } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Directive({
   selector: "input[trh-input]",
-  host:{
-    "class":"form-control"
+  host: {
+    "class": "form-control",
+    "[placeholder]": "placeholder"
   }
 })
-export class TrhInputComponent {
+export class TrhInputComponent implements AfterViewInit {
 
+  @Input() placeholder = "";
+
+  constructor(private translateService: TranslateService) {
+  }
+
+  ngAfterViewInit() {
+    if (this.placeholder !== "")
+      this.placeholder = this.translateService.instant(this.placeholder);
+  }
 }
