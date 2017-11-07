0ee542fe0cd617c46b23add5948b2e3985708b7a
Mr. Happiness <m.roostaee@outlook.com>
2017-11-07 13:04:17 +0330

    Bug fixed - Make spa buildable

--- src/app/@core/data/api.service.ts
+++ src/app/@core/data/api.service.ts
@@ -10,53 +10,53 @@ import { Helper } from "../utils";
 
 @Injectable()
 export class ApiService {
-    private headers: Headers;
+  private headers: Headers;
 
-    constructor(public dataService: DataService) {
-        dataService.baseAddress = Configuration.ApiUrl + "json/reply/";
-        this.headers = new Headers();
-        this.headers.append("Content-Type", "application/json");
-        this.headers.append("Accept", "application/json");
+  constructor(public dataService: DataService) {
+    dataService.baseAddress = Configuration.ApiUrl + "json/reply/";
+    this.headers = new Headers();
+    this.headers.append("Content-Type", "application/json");
+    this.headers.append("Accept", "application/json");
+  }
+
+  getEntities<T>(data: IReturn<QueryResponse<T>>): Observable<QueryResponse<T>> {
+
+    return this.internalSend(data);
+  }
+
+  // protected saveEntity<T>(data: IReturn<EntityCreatedResponse<T>>): Observable<EntityCreatedResponse<T>> {
+  //     return this.internalSend(data);
+  // }
+
+  get<T>(data: IReturn<T>): Observable<T> {
+
+    return this.internalSend(data);
+  }
+
+  send<T>(data: IReturn<T>): Observable<T> {
+    return this.internalSend(data);
+  }
+
+  private internalSend<T>(data: IReturn<T>): Observable<T | any> {
+    const httpMethod = Helper.getHttpMethod(data);
+    let requestMethod = RequestMethod.Get;
+    switch (httpMethod) {
+      case "POST":
+        requestMethod = RequestMethod.Post;
+        break;
+      case "PUT":
+        requestMethod = RequestMethod.Put;
+        break;
     }
 
-    getEntities<T>(data: IReturn<QueryResponse<T>>): Observable<QueryResponse<T>> {
 
-        return this.internalSend(data);
-    }
-
-    // protected saveEntity<T>(data: IReturn<EntityCreatedResponse<T>>): Observable<EntityCreatedResponse<T>> {
-    //     return this.internalSend(data);
-    // }
-
-    get<T>(data: IReturn<T>): Observable<T> {
-
-        return this.internalSend(data);
-    }
-
-    send<T>(data: IReturn<T>): Observable<T> {
-        return this.internalSend(data);
-    }
-
-    private internalSend<T>(data: IReturn<T>): Observable<T> {
-        const httpMethod = Helper.getHttpMethod(data);
-        let requestMethod = RequestMethod.Get;
-        switch (httpMethod) {
-            case "POST":
-                requestMethod = RequestMethod.Post;
-                break;
-            case "PUT":
-                requestMethod = RequestMethod.Put;
-                break;
-        }
-
-
-        return this.dataService.request(requestMethod, data.getTypeName(), Serializable.toJSON(data), new RequestOptions({headers: this.headers})).map(
-            res => {
-                const type = data.createResponse();
-                if (type)
-                    return Serializable.fromJSON(type, res);
-                return res;
-            });
-    }
+    return this.dataService.request(requestMethod, data.getTypeName(), Serializable.toJSON(data), new RequestOptions({headers: this.headers})).map(
+      res => {
+        const type = data.createResponse();
+        if (type)
+          return Serializable.fromJSON(type, res);
+        return res;
+      });
+  }
 
 }
